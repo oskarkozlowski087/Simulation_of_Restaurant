0c10457562b448feeaa8c8ebde83e476024eea4f
oskarkozlowski087 <oskarkozlowski087@gmail.com>
2026-06-01 19:17:07 +0200

    Wzbogacenie klas Moving Agent i Waiter o metody

--- src/agents/MovingAgent.java
+++ src/agents/MovingAgent.java
@@ -1,4 +1,25 @@
 package agents;
 
-public class MovingAgent {
+public abstract class MovingAgent {
+    protected int x;
+    protected int y;
+    protected int tarX;
+    protected int tarY;
+    protected boolean isOccupied;
+
+    public MovingAgent(int x, int y){
+        this.x = x;
+        this.y = y;
+        this.isOccupied = false;
+    }
+
+    public boolean isOccupied(){
+        return isOccupied;
+    }
+
+    public void scanBoard(){
+
+    }
+
+
 }
--- src/agents/Waiter.java
+++ src/agents/Waiter.java
@@ -1,4 +1,27 @@
 package agents;
 
-public class Waiter {
+//import environment.Table
+//import environment.Buffer
+//import models.Order
+
+public class Waiter extends MovingAgent {
+    public Waiter(int x, int y){
+        super(x,y);
+    }
+    public void pickUpOrder(){ //Table table
+        this.isOccupied = true;
+        System.out.println("Kelner odebrał zamówienie od stolika");
+    }
+    public void dropOrderAtBuffer(){    //Buffer buffer
+        System.out.println("Kelner zostawił zamówienie na bufferze dla kucharza");
+        this.isOccupied = false;
+    }
+    public void pickOrderFromBuffer(){   //Buffer buffer
+        System.out.println("Kelnerz odebrał zamówienie z buffera.");
+        this.isOccupied = true;
+    }
+    public void deliverOrder(){ //Table table
+        System.out.println("Danie zostało podane!");
+        this.isOccupied = false;
+    }
 }
