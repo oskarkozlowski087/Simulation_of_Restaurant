f4308c9f48a6806d0e37d99aac6f98edeb2731cd
TheAdmiral12 <kacperpatyk29@gmail.com>
2026-06-02 14:01:02 +0200

    Dodano metody i atrybuty klas Board oraz Cell w celu podłożenia fundamentów pod dalszą implementację

--- src/main/java/core/Board.java
+++ src/main/java/core/Board.java
@@ -1,5 +1,49 @@
 package core;
 
-public class Board(){
+import environment.Cell;
+import environment.CellType;
+import agents.MovingAgent;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Board{
+private final int width;
+private final int height;
+private final Cell[][] grid;
+private final List<MovingAgent> activeAgents;
+
+    //Konstruktor podający wzrost i wysokość, tworzy nową planszę
+public Board(int width, int height) {
+    this.width = width;
+    this.height = height;
+    this.grid = new Cell[width][height];
+    this.activeAgents = new ArrayList<>();
+}
+
+ // Metoda odpowiedzialna za tworzenie planszy
+private void initializeGrid() {
+}
+
+public Cell getCell (int x, int y){
+    return null;
+}
+
+    //sprawdzenie, czy agent może wejść na pole
+public boolean canMoveTo(int x, int y){
+    Cell cell = getCell(x,y);
+    return cell != null && cell.isWalkable() && cell.getOccupant() == null;
+}
+// metoda rejestruje agenta i stawia go na odpowiedniej komorce
+public void registerAgent(MovingAgent agent, int startX, int startY ) {
+}
+
+public int getWidth() {
+    return width;
+}
+public int getHeight() {
+    return height;
+}
+public List<MovingAgent> getActiveAgents() {
+    return activeAgents;
+}
 }
--- src/main/java/environment/Cell.java
+++ src/main/java/environment/Cell.java
@@ -1,4 +1,37 @@
 package environment;
+import agents.MovingAgent;
 
 public class Cell {
+    private final int x;
+    private final int y;
+    private final CellType type;
+    private MovingAgent occupant;
+    private Table table;
+    private boolean isStove;
+
+    public Cell(int x, int y, CellType type){
+        this.x = x;
+        this.y = y;
+        this.type = type;
+        this.occupant = null;
+        this.table = null;
+        this.isStove = false;
+    }
+
+    public boolean isWalkable() {
+        return this.table == null;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+    public CellType getType() { return type; }
+
+    public MovingAgent getOccupant() { return occupant; }
+    public void setOccupant(MovingAgent agent) { this.occupant = agent; }
+
+    public Table getTable() { return table; }
+    public void setTable(Table table) { this.table = table; }
+
+    public boolean isStove() { return isStove; }
+    public void setStove(boolean isStove) { this.isStove = isStove; }
 }
